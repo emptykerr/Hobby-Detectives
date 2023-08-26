28e61fa087a3a36390a891440cda85d3089de733
Tokeley <137751232+Tokeley@users.noreply.github.com>
2023-08-27 10:26:30 +1200

    Removed ongoing background as it used AWT

--- Swen225-A1/Java/GUI/Ongoing.java
+++ Swen225-A1/Java/GUI/Ongoing.java
@@ -10,7 +10,6 @@ import java.awt.image.BufferedImage;
 
 public class Ongoing extends JFrame {
 
-    private BufferedImage wallpaperBackground = LoadSave.GetSpriteAtlas(LoadSave.ONGOING_WALLPAPER);
     public int backgroundWidth, backgroundHeight;
     
     public Ongoing (Game game){
@@ -25,16 +24,6 @@ public class Ongoing extends JFrame {
         int width = Game.GAME_WIDTH;
         int height = Game.GAME_HEIGHT;
 
-        double imgWidth = wallpaperBackground.getWidth();
-        double imgHeight = wallpaperBackground.getHeight();
-
-        float widthScale = (float) (width / imgWidth);
-        float heightScale = (float) (height / imgHeight);
-
-        backgroundWidth = (int)((wallpaperBackground.getWidth() * widthScale) * Game.SCALE);
-        backgroundHeight = (int)((wallpaperBackground.getHeight() * heightScale) * Game.SCALE);
-        
-        g.drawImage(wallpaperBackground, Game.getPanelWidth()/2 - backgroundWidth/2, 0, backgroundWidth , backgroundHeight, null);
     }
 
     public void update(){}
